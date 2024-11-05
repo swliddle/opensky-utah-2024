07673353cc8525beeedf7be66254754ff367676b
Steve Liddle <liddle@byu.edu>
2024-11-05 08:41:37 -0700

    Initial Commit

---
+++ OpenSkyUtah/OpenSkyUtahApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSkyUtahApp.swift
+//  OpenSkyUtah
+//
+//  Created by Stephen Liddle on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenSkyUtahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4bc4c422e2ba5ec4ba995b04aa94d39757e4f08b
Steve Liddle <liddle@byu.edu>
2024-11-05 09:54:05 -0700

    Finished Day 18 in-class demo.

--- OpenSkyUtah/View/OpenSkyUtahApp.swift
+++ OpenSkyUtah/View/OpenSkyUtahApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct OpenSkyUtahApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(openSkyService: OpenSkyService())
         }
     }
 }
